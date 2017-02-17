bcc5f5de4669e05e3aa3304243ed1acf70f5efc7
SunShinewyf <2632807692@qq.com>
2017-02-17 15:33:13 +0800

    use the webpack-dev-server

--- app/component.js
+++ app/component.js
@@ -4,6 +4,6 @@
  */
 module.exports = function () {
     var element = document.createElement('h1');
-    element.innerHTML = 'hello world';
+    element.innerHTML = 'hello worlddsasdga';
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@
 var path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app/index.js'),
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'app/index.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
