65ccc684a54419888d1ae15f6995ae5e446b76d5
SunShinewyf <2632807692@qq.com>
2017-02-22 18:49:26 +0800

    the other way

---
+++ demo1/example1.js
@@ -0,0 +1,5 @@
+module.exports = {
+    sayHello = function () {
+        console.log('hello world1')
+    }
+}
---
+++ demo1/webpack.config.js
@@ -0,0 +1,13 @@
+/**
+ * author:SunShinewyf
+ * date:2017-02-22
+ */
+module.exports = {
+    entry: {
+        'example1': 'example1',
+        'example2': 'example2'
+    },
+    output: {
+        filename: '[name]-[hash].js'
+    }
+};
